0535d96aac0bec1e635bc3cffb78117b83c9a931
Hibbert-pku <lixibai2@126.com>
2015-12-09 18:59:57 +0800

    remove log and clean code

--- cuda/cuda.cc
+++ cuda/cuda.cc
@@ -1,18 +1,13 @@
 #include<stdio.h>
-#ifdef __CUDA__
+#ifdef useCUDA
 #include <cuda_runtime.h>
 __global__ void kernel() {
-  printf("gpu!!!!");
+  printf("in kernel\n");
 }
 #endif
 void test() {
-#ifdef __CUDA__
-  dim3 block(1);
+#ifdef useCUDA
   kernel<<<1,1>>>();
   cudaDeviceSynchronize();
 #endif
-#ifdef use_CUDA
-  printf("right\n");
-#endif
-  printf("cuda here\n");
 }
